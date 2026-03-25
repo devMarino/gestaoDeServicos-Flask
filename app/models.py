73db54d5e0f193b31135706639bd77c27f175bda
devMarino <marcelomcb24@gmail.com>
2026-03-25 19:46:35 -0300

    feat: implement database schema

---
+++ app/models.py
@@ -0,0 +1,53 @@
+from app import db
+from datetime import datetime, timezone
+import enum
+
+class StatusAgendamento(enum.Enum):
+    PENDENTE = 'PENDENTE'
+    CONCLUIDO = 'CONCLUIDO'
+    CANCELADO = 'CANCELADO'
+    AUSENTE = 'AUSENTE'
+
+class StatusFuncionario(enum.Enum):
+    ATIVO = 'Ativo'
+    INATIVO = 'Inativo'
+    
+class Cliente(db.Model):
+    __tablename__ = 'clientes'
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(200), nullable=False)
+    telefone = db.Column(db.String(20),nullable=False)
+    email = db.Column(db.String(320), nullable=False, unique=True)
+    senha = db.Column(db.String(128),nullable=False)
+    data_cadastro = db.Column(db.Date, default=lambda: datetime.now(timezone.utc), nullable=False)
+
+class Funcionario(db.Model):
+    __tablename__ = 'funcionarios'
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(200), nullable=False)
+    especialidade = db.Column(db.String(500), nullable=True)
+    status = db.Column(db.Enum(StatusFuncionario), default=StatusFuncionario.INATIVO, nullable=False)
+
+class Agendamento(db.Model):
+    __tablename__ = 'agendamentos'
+    id = db.Column(db.Integer, primary_key=True)
+    data_hora = db.Column(db.DateTime, nullable=False)
+    status = db.Column(db.Enum(StatusAgendamento), default=StatusAgendamento.PENDENTE,nullable=False)
+
+    cliente_id = db.Column(db.Integer, db.ForeignKey('clientes.id'), nullable=False)
+    funcionario_id = db.Column(db.Integer, db.ForeignKey('funcionarios.id'), nullable=False)
+
+class AtendimentoItem(db.Model):
+    __tablename__ = 'atendimento_itens'
+    id = db.Column(db.Integer, primary_key=True)
+    valor_aplicado = db.Column(db.Numeric(10, 2), nullable=False)
+    
+    agendamento_id = db.Column(db.Integer, db.ForeignKey('agendamentos.id'), nullable=False)
+    servico_id = db.Column(db.Integer, db.ForeignKey('servicos.id'), nullable=False)
+
+class Servico(db.Model):
+    __tablename__ = 'servicos'
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(200), nullable=False)
+    preco = db.Column(db.Numeric(10, 2), nullable=False)
+    duracao_estimada = db.Column(db.Integer) 
