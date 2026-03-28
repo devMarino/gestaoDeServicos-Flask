f3f25b39eef151aae01c415a4f307b14c8993dcc
devMarino <marcelomcb24@gmail.com>
2026-03-28 18:24:59 -0300

    feat: CRUD de clientes
    Contém validação de email
    
    Contém tratamento de erros

--- app/routes/clientes.py
+++ app/routes/clientes.py
@@ -25,17 +25,21 @@ def get_all_client():
     
 
 @clientes_bp.route('/novo', methods=['POST'])
-def criar_cliente():
+def create_client():
     try:
         data = request.get_json()
-        
         if not data:
             return jsonify({'error': 'requisição sem corpo JSON'}), 400
-
+        
+        email = data.get('email')
+        user_email = Cliente.query.filter_by(email=email).first()
+        # evita duplicata de email
+        if user_email:
+            return jsonify({'erro': 'Email já cadastrado!'}), 409
         nome = data.get('nome')
         telefone = data.get('telefone')
-        email = data.get('email')
         senha = data.get('senha')
+
         # data_cadastro já preenche sozinho
         if not nome or not email or not telefone or not senha:
             return jsonify({'erro': 'Preencha todos os campos obrigatórios'}), 400 
@@ -48,7 +52,57 @@ def criar_cliente():
         db.session.commit()
 
         return jsonify({'message': 'cliente cadastrado com sucesso!', 'id': novo_cliente.id }), 201
+        
     # caso dê errado, desfaz tudo
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({'error': str(e)}), 500
+    
+@clientes_bp.route('/<int:id>', methods=['GET'])
+def get_specific_client(id):
+    try:
+        # get_or_404  = se o id não existir, o flask retorna 404 not found
+        user = Cliente.query.get_or_404(id)
+        return jsonify({
+            'id': user.id,
+            'nome': user.nome,
+            'email': user.email,
+            'telefone': user.telefone}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@clientes_bp.route('/<int:id>', methods=['PUT'])
+def put_specific_client(id):
+    try:
+        user = Cliente.query.get_or_404(id)
+        data = request.get_json()
+
+        novo_email = data.get('email')
+
+        if novo_email and novo_email != user.email:
+            # filtra se email já possui cadastro
+            email_existe = Cliente.query.filter_by(email=novo_email).first()
+            if email_existe:
+                return jsonify({'error':'E-mail já possui cadastro'}), 409
+            
+        user.nome = data.get('nome', user.nome)
+        user.email = novo_email if novo_email else user.email
+        
+        user.telefone = data.get('telefone', user.telefone)
+        
+        db.session.commit()
+        return jsonify({'message': 'Cliente atualizado com sucesso!'}), 200
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({'error': str(e)}), 500
+
+@clientes_bp.route('/<int:id>', methods=['DELETE'])
+def delete_specific_client(id):
+    try:
+        user = Cliente.query.get_or_404(id)
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({'message': 'cliente removido com sucesso!'}), 200
     except Exception as e:
         db.session.rollback()
         return jsonify({'error': str(e)}), 500
